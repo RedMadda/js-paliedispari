afa07b9c56b6ca6038f9d916568ddbbfe78b539a
RedMadda <maddalenarossaro91@gmail.com>
2021-06-07 16:52:52 +0200

    palindromo corretto

--- js/script.js
+++ js/script.js
@@ -3,22 +3,16 @@
 
 
 // inizio funzione
-function checkPal(word){
-    // rendo tutto minuscolo
-    
-    word = word.substr(0).toLowerCase();
-    
-    //  controllo se metà e metà dal centro sono uguali
-    
-    for (var i = 0; i < word.length/2; i++){
-        if(word[i] != word[length -1 -i]){ 
+function checkPal(word) {
+    word = word.toLowerCase();
+    for (var i = 0; i < word.length/2; i++) {
+      if (word[i] !== word[word.length - 1 - i]) {
           return false;
-        } else if (word[i] == word[length -1 +i]){
-            return true;
-        }
-    }  
+      }
     }
-    
+    return true;
+   }
+     
     // inserire parola
     
     var parola = prompt("Inserisci una parola.");
